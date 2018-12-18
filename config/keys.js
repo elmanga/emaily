debc600ba2cc208200abfd70658f5bcec007641c
Alvaro Manganello <amanganello@altimetrik.com>
2018-12-18 11:23:08 -0300

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// decides what set of credentials to return 
+if (process.env.NODE_ENV === 'production') {
+    // we are in production, so we return this set of keys
+    module.exports = require('./prod');
+} else {
+    // we are in dev, so we return this set of keys
+    module.esports = require('./dev');
+}
